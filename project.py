25ceceaabe779be484eb5ccafd14e1080eca0f87
Alexandra Macuga <sasenah@gmail.com>
2019-04-27 13:18:29 +0100

    load modules, load dataset, show values

--- project.py
+++ project.py
@@ -1,3 +1,26 @@
 # Alexandra Macuga, 2019-04-28
 # This is my solution to the Project 2019 for the module Programming and Scripting at GMIT.
 
+# Load modules
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load dataset
+file = "iris.csv"
+features = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+dataset = pd.read_csv(file, names=features)
+
+# Show values of each column:
+# - number of values
+# - mean of the values
+# - standard deviation
+# - minimum of the values
+# - lower percentile(25th)
+# - median (50th percentile)
+# - upper percentile (75th)
+# - maximum of the values
+print(dataset.describe())
+
+
+
