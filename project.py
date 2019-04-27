51b5fdcdedf73cdde9b2081794e530ee62d4e4b3
Alexandra Macuga <sasenah@gmail.com>
2019-04-27 20:44:49 +0100

    Added scatter plot matrix

--- project.py
+++ project.py
@@ -1,17 +1,18 @@
 # Alexandra Macuga, 2019-04-28
 # This is my solution to the Project 2019 for the module Programming and Scripting at GMIT.
+# This progam contains my investigations into Iris data set.
 
 # Load modules
 import pandas as pd
+from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
-import numpy as np
 
 # Load dataset
 file = "iris.csv"
 features = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 dataset = pd.read_csv(file, names=features)
 
-# Show values of each column:
+# Compute and show the following values for each feature (column):
 # - number of values
 # - mean of the values
 # - standard deviation
@@ -26,3 +27,6 @@ print(dataset.describe())
 dataset.hist()
 plt.show()
 
+# Create a scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
